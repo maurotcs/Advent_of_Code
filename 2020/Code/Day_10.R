18f1f15853a86b4264c70ef21ff1aa8a23cb4bd2
maurotcs <maurotcs@gmail.com>
2024-12-05 10:52:58 -0300

    10/25 - 2020

--- 2020/Code/Day_10.R
+++ 2020/Code/Day_10.R
@@ -126,20 +126,26 @@ prod(res)
 # You glance back down at your bag and try to remember why you brought so many adapters; there must be more than a trillion valid ways to arrange them! Surely, there must be an efficient way to count the arrangements.
 # What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
 
-get_diff = function(vec){
-  vec = sort(vec, decreasing = FALSE)
-  vec = c(0, vec, max(vec)+3)
-  out = diff(vec)
-  
-  return(out)
+adapters = c(0, sort(input, decreasing = FALSE), max(input)+3)
+jolts = diff(adapters)
+
+d3 = which(jolts == 3)
+jolt1 = list()
+j1=1
+start=1
+for(i in 1:length(d3)){
+  if(d3[i] > start){
+    jolt1[[j1]] = as.integer(adapters[start:d3[i]])
+    j1 = j1+1
+  }
+  start = d3[i]+1
 }
 
-count_arrang = function(vec){
-  foo = get_diff(vec)
-  
-  
-  return(out)
-}
+permutations2 = 2^(sapply(jolt1, length)-2)
+permutations2[which(permutations2==8)] = 7
+
+prod(permutations2)
+
 
 
 
